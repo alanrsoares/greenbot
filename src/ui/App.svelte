8fd71b2e210c7ebe0607e1fd9845040569d81b6c
Alan R. Soares <alanrsoars@gmail.com>
2021-08-04 11:58:53 +1200

    chore: include package name and version info

--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { useQuery } from "@sveltestack/svelte-query";
+  import DiNpm from "svelte-icons/di/DiNpm.svelte";
+
   import type { TabKind } from "domain/types";
 
   import { QUERIES } from "../domain/constants";
@@ -27,32 +29,41 @@
 
 <Layout>
   <div class="w-full grid gap-4">
-    <div
-      class="border-2 border-granny-smith-apple rounded-xl flex justify-between overflow-hidden"
-    >
-      <button
-        data-value="dependencies"
-        class="p-4 cursor-pointer flex-1"
-        class:bg-castleton-green={selectedTab === "dependencies"}
-        on:click={handleTabClick}
-      >
-        Dependencies
-      </button>
-      <button
-        data-value="dev-dependencies"
-        class="p-4 cursor-pointer flex-1 border-l border-granny-smith-apple"
-        class:bg-castleton-green={selectedTab === "dev-dependencies"}
-        on:click={handleTabClick}
-      >
-        Dev Dependencies
-      </button>
-    </div>
-
     {#if $queryResult.isLoading}
       <div class="mx-auto">Loading dependencies...</div>
     {/if}
 
     {#if $queryResult.data}
+      <div
+        class="bg-[#cb3837] px-4 rounded-xl text-white flex items-center justify-center font-medium"
+      >
+        <div class="h-12 mr-2 pt-0.5">
+          <DiNpm />
+        </div>
+        <div>
+          {$queryResult.data.name} - {$queryResult.data.version}
+        </div>
+      </div>
+      <div
+        class="border-2 border-granny-smith-apple rounded-xl flex justify-between overflow-hidden"
+      >
+        <button
+          data-value="dependencies"
+          class="p-4 cursor-pointer flex-1"
+          class:bg-castleton-green={selectedTab === "dependencies"}
+          on:click={handleTabClick}
+        >
+          Dependencies
+        </button>
+        <button
+          data-value="dev-dependencies"
+          class="p-4 cursor-pointer flex-1 border-l border-granny-smith-apple"
+          class:bg-castleton-green={selectedTab === "dev-dependencies"}
+          on:click={handleTabClick}
+        >
+          Dev Dependencies
+        </button>
+      </div>
       {#if selectedTab === "dependencies"}
         <Dependencies
           label="Dependencies"
