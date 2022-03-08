e08e9b8dd4e4c98a42325ecb2a4be29778812d21
Alan Soares <alanrsoars@gmail.com>
2022-03-08 14:12:52 +1300

    chore: type custom directive event

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+declare namespace svelte.JSX {
+  interface DOMAttributes<T> {
+    onoutsideclick?: CompositionEventHandler<T>;
+  }
+}
